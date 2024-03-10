684609ecff8407ee3c7651775cd2b2379ce9050c
Nart <113383731+nartna123@users.noreply.github.com>
2024-03-10 16:29:33 +0200

    Init Tyiescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import {PI} from "./consts";
+
+function main(): void {
+    const hello: string = "Hello, world!"
+    console.log("Greeting:", hello);
+
+    console.log("Check export: ", PI);
+}
+
+main();
