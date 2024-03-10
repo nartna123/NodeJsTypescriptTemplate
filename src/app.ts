a4e40fd53a44f7556668a72002b996ac538bf7ce
Nart <113383731+nartna123@users.noreply.github.com>
2024-03-10 17:17:20 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,40 @@
-import express, {Request, Response} from 'express';
+import express, {Express, Request, Response} from 'express';
 import {PI} from "./consts";
 export function startExpressServer() {
     // Create Express app
-    const app = express();
-    const port = 3000;
+    const app: Express = express();
+    const port: number = 3000;
+
+    // Variables
+    let counter: number = 0;
 
     // Define a route
     app.get('/', (req: Request, res: Response) => {
-        res.send('Hello World!');
+        res.send('Alive!');
+    });
+
+    app.get('/check', (req: Request, res: Response) => {
+        res.send('Check!');
     });
 
     app.get('/pi', (req: Request, res: Response) => {
         res.send(`PI is approximately ${PI}`);
+    });
+
+    app.get('/hello', (req: Request, res: Response) => {
+        const { firstName, lastName } = req.query;
+
+        if (!firstName || !lastName) {
+            return res.status(400).send('Both first name and last name are required.');
+        }
+
+        const greeting = `Hello ${firstName} ${lastName}`;
+        res.send(greeting);
+    });
+
+    app.get('/counter', (req: Request, res: Response) => {
+        res.send(`Counter is at ${counter}`);
+        counter += 1;
     });
 
     // Start the Express server
